86783396dd8602fb3225bc71544fabeb5bef2591
¨Ramiro <ramirofraysse@gmail.com>
2026-01-31 16:00:54 -0300

    update information

--- models/sectionCoverpage.ts
+++ models/sectionCoverpage.ts
@@ -1,7 +1,7 @@
 export const COVERPAGE_TEXT = {
     TITLE: "Ramiro Fraysse",
     DESCRIPTION: {
-        sp: "Hola! Mi nombre es Ramiro. A lo largo de mi carrera he tenido el placer de participar en equipos de alta performance y desarrollar productos de calidad con agilidad y eficiencia. Me gusta jugar en equipo, destaco por ser proactivo y estar dispuesto a colaborar para hacer realidad tus ideas.",
+        sp: "Hola! Mi nombre es Ramiro. A lo largo de mi carrera he tenido el placer de participar en excelentes equipos y desarrollar productos de calidad con agilidad y eficiencia. Me gusta jugar en equipo, destaco por ser proactivo y estar dispuesto a colaborar para hacer realidad tus ideas.",
         en: "Hi! My name is Ramiro. Throughout my career, I have had the pleasure of participating in high-performance teams and developing quality products with agility and efficiency. I enjoy being a team player, and I stand out for being proactive and collaborating to make your ideas a reality.",
     },
     SUBTITLE1: {
